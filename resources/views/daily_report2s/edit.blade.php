e1d7bafd8cca3af589b4ec2059ed34ba9c8b932f
tanaka <tanakakenji@126.com>
2023-03-01 17:48:58 +0800

    20230301

--- resources/views/daily_report2s/edit.blade.php
+++ resources/views/daily_report2s/edit.blade.php
@@ -21,28 +21,28 @@
       <input type="string" name="employee_1" class="typing_place daily_report" placeholder="吴玉霞" value="{{$daily_report2->employee_1}}">
     </label>
     <label>
-      <input type="string" name="employee_2" class="typing_place daily_report" placeholder="臧力群" value="{{$daily_report2->employee_2}}"> value="{{old('employee_2')}}">
+      <input type="string" name="employee_2" class="typing_place daily_report" placeholder="臧力群" value="{{$daily_report2->employee_2}}"> 
     </label>
     <label>
-      <input type="string" name="employee_3" class="typing_place daily_report" placeholder="徐倩" value="{{$daily_report2->employee_3}}"> value="{{old('employee_3')}}">
+      <input type="string" name="employee_3" class="typing_place daily_report" placeholder="徐倩" value="{{$daily_report2->employee_3}}">
     </label>
     <label>
-      <input type="string" name="employee_4" class="typing_place daily_report" placeholder="孙翔" value="{{$daily_report2->employee_4}}"> value="{{old('employee_4')}}">
+      <input type="string" name="employee_4" class="typing_place daily_report" placeholder="孙翔" value="{{$daily_report2->employee_4}}">
     </label>
     <label>
-      <input type="string" name="employee_5" class="typing_place daily_report" placeholder="曲文佳" value="{{$daily_report2->employee_5}}"> value="{{old('employee_5')}}">
+      <input type="string" name="employee_5" class="typing_place daily_report" placeholder="曲文佳" value="{{$daily_report2->employee_5}}">
     </label>
     <label>
-      <input type="string" name="employee_6" class="typing_place daily_report" placeholder="王晓娜" value="{{$daily_report2->employee_6}}"> value="{{old('employee_6')}}">
+      <input type="string" name="employee_6" class="typing_place daily_report" placeholder="王晓娜" value="{{$daily_report2->employee_6}}">
     </label>
     <label>
-      <input type="string" name="employee_7" class="typing_place daily_report" placeholder="王玉娇" value="{{$daily_report2->employee_7}}"> value="{{old('employee_7')}}">
+      <input type="string" name="employee_7" class="typing_place daily_report" placeholder="王玉娇" value="{{$daily_report2->employee_7}}"> 
     </label>
     <label>
-      <input type="string" name="employee_8" class="typing_place daily_report" placeholder="法務部" value="{{$daily_report2->employee_8}}"> value="{{old('employee_8')}}">
+      <input type="string" name="employee_8" class="typing_place daily_report" placeholder="法務部" value="{{$daily_report2->employee_8}}">
     </label>
     <label>
-      <input type="string" name="employee_9" class="typing_place daily_report" placeholder="-" value="{{$daily_report2->employee_9}}"> value="{{old('employee_9')}}">
+      <input type="string" name="employee_9" class="typing_place daily_report" placeholder="-" value="{{$daily_report2->employee_9}}">
     </label>
     <button type="submit" class="btn btn-warning btn-sm shadow-lg">編集</button>
   </form>
